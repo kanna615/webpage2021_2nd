95d79e43170785223255584463598f9c5d3671f2
kanna615 <e5429@gm.kochi-ct.jp>
2021-12-07 16:18:59 +0900

    Create uptab.php

---
+++ uptab.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+?>
+<html>
+<head>
+    <title>カード情報書き換え</title>
+    <meta charset="utf-8">
+</head>
+<body>
+
+<?php
+$pass = "Nstyle";
+if(isset($_SESSION["pass"]) && $_SESSION["pass"]==$pass){
+
+if(isset($_POST["action"])){
+/*▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲
+    //DB接続処理
+▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲*/
+$dbhost="ec2-52-205-61-60.compute-1.amazonaws.com";
+$dbname="dcg3sava0u5ic6";
+$dbuser="tlvwasltznluaj";
+$dbpass="828dff754cd6db621b4756042f5c3b47623ca41fcf8cf4e817fa7efffef3a21c";
+$dbtype="pgsql";
+$dsn = "$dbtype:dbname=$dbname host=$dbhost port=5432";
+try{
+    $pdo=new PDO($dsn,$dbuser,$dbpass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+}catch(PDOException $Exception){
+    print "エラー";
+}
+/*▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲
+▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲*/
+$Lname=$_POST["last_name"];
+$Fname=$_POST["first_name"];
+$work=$_POST["work"];
+$Cid=$_POST["card_id"];
+$sql_update="UPDATE sample_member SET last_name=?, first_name=?, work=? WHERE card_id= ?";
+
+try{
+    $stmh=$pdo->prepare($sql_update);
+    $stmh->execute(array($Lname,$Fname,$work,$Cid));
+    print "更新しました。<br>";
+}catch(PDOException $Exception){
+    print　"エラー";
+}
+
+?>
+<a href="" onclick="document.card.submit();return false;">戻る</a>
+<form method="post" name="card" action="カード管理.php">
+    <input type="hidden" name="pass" value=<?=$_SESSION["pass"]?>>
+</form>
+<?php
+}
+
+}
+?>
+
+</body>
+</html>
