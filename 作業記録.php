106487b74b0f0da6ab6d11f7f9424c4419e8d21d
kanna615 <e5429@gm.kochi-ct.jp>
2021-12-09 00:30:41 +0900

    Create 作業記録.php

--- 作業記録.php
+++ 作業記録.php
@@ -157,11 +157,11 @@ function Harvest_color($row_){
 <?php
 function csv_tablename($KEY1_ , $KEY21_ , $KEY31_ , $KEY32_ , $KEY33_){
     echo '<form name="formcsv" method="post" action="記録CSV処理.php">';
-        echo "<input type='hidden' name='key1' value='{$KEY1_}'>";
-        echo "<input type='hidden' name='key21' value='{$KEY21_}'>";
-        echo "<input type='hidden' name='key31' value='{$KEY31_}'>";
-        echo "<input type='hidden' name='key32' value='{$KEY32_}'>";
-        echo "<input type='hidden' name='key33' value='{$KEY33_}'>";
+        echo "<input type='hidden' name='key1' value='<?php echo {$KEY1_};?>'>";
+        echo "<input type='hidden' name='key21' value='<?php echo {$KEY21_};?>'>";
+        echo "<input type='hidden' name='key31' value='<?php echo {$KEY31_};?>'>";
+        echo "<input type='hidden' name='key32' value='<?php echo {$KEY32_};?>'>";
+        echo "<input type='hidden' name='key33' value='<?php echo {$KEY33_};?>'>";
         echo "<input type='submit' value='CSVファイルを保存'>";
     echo '</form>';
 
