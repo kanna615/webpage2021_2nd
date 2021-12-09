5330b238f6cdd39148e25a7507d1a8dc6df948c2
kanna615 <e5429@gm.kochi-ct.jp>
2021-12-09 12:59:42 +0900

    Create 作業記録.php

--- 作業記録.php
+++ 作業記録.php
@@ -3,6 +3,15 @@ session_start();
 ?>
 <html>
 <head>
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+ 
+<!-- Bootstrap Javascript(jQuery含む) -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 <title>作業記録管理ページ</title>
 <meta charset=utf-8>
 </head>
@@ -47,6 +56,7 @@ else {
 <a href="グラフ表示ページ.php">グラフ表示</a><br>
 <form name="form1" method="post" action="作業記録.php">
        
+        <div class="panel-heading">検索フォーム</div>
         <font size="4" color="#000000">名前で検索:</font><br>
         <input type="text" name="search_key" value="<?php echo $set_name; ?>"><br> 
         
