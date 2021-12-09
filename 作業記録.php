6fc6af03b26d27e8fdc40fd3c5f6d8e593947c43
kanna615 <e5429@gm.kochi-ct.jp>
2021-12-09 14:16:05 +0900

    Create 作業記録.php

--- 作業記録.php
+++ 作業記録.php
@@ -3,14 +3,9 @@ session_start();
 ?>
 <html>
 <head>
-
 <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
- 
-<!-- Bootstrap Javascript(jQuery含む) -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 <title>作業記録管理ページ</title>
 <meta charset=utf-8>
@@ -61,14 +56,12 @@ else {
         <div class="panel-heading">検索フォーム</div>
         <div class="panel-body">
             <form class="form-horizontal" name="form1" method="post" action="作業記録.php">
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">名前</label>
-                    <div class="row">
-                        <div class="col-sm-10">
-                            <input class="form-control"　type="text" name="search_key" value="<?php echo $set_name; ?>">
-                        </div>
-                    </div>
-                </div>
+                <div class="col-auto">
+                    <label cfor="inputPassword6" class="col-form-label">名前</label>
+                </div>  
+                <div class="col-auto">
+                    <input class="form-control"　type="text" name="search_key" value="<?php echo $set_name; ?>">
+                </div>   
                 <div class="form-group">
                     <label class="col-sm-2 control-label">作業内容</label>
                     <div class="col-sm-10 radio">
@@ -642,6 +635,9 @@ print "年月または名前を指定してください。<br>";*/
 <?php
 }
 ?>
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
 </html>
