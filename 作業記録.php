43f69dca9817e492150f392965b6edb284f6e6da
kanna615 <e5429@gm.kochi-ct.jp>
2021-12-15 16:11:08 +0900

    Create 作業記録.php

--- 作業記録.php
+++ 作業記録.php
@@ -109,9 +109,10 @@ session_start();
               </select>
             </div>
             <!-- <label for="colFormLabelSm" class="col-sm-1 col-form-label col-form-label-sm">年</label> -->
-            <div class="col">年</div>
+            <!-- <div class="col">年</div> -->
             <div class="col-auto">
-              <select class="form-select pl-0 ml-0" name="month">
+              年
+              <select class="form-select" name="month">
                 <option value="<?php echo $set_month; ?>" selected><?php echo $set_month; ?></option>
                 <option value="">指定なし</option>
                 <?php
