8f099ebbf1e90e3f7d115a01644ba0a32c7b18a6
kanna615 <e5429@gm.kochi-ct.jp>
2021-12-15 14:43:55 +0900

    Create 作業記録.php

--- 作業記録.php
+++ 作業記録.php
@@ -72,8 +72,8 @@ session_start();
     <a href="グラフ表示ページ.php">グラフ表示</a><br>
    <form name="form1" method="post" action="作業記録.php"> -->
 
-    <div class="container" class="pt-5">
-      <div class="p-4 p-md-5 mb-4 text-white rounded bg-secondary">
+    <div class="container">
+      <div class="p-4 p-md-5 mb-4 mt-2 text-white rounded bg-secondary">
         <form class="form-horizontal" name="form1" method="post" action="作業記録.php">
           <div class="row mb-3 mt-5">
             <label for="inputname" class="col-sm-1 col-form-label">名前</label>
