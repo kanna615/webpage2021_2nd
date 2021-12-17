382a6fe27f18357830f857f2d7697aaf8db9a105
kanna615 <e5429@gm.kochi-ct.jp>
2021-12-17 15:55:56 +0900

    deploy test

--- index.php
+++ index.php
@@ -132,7 +132,7 @@ session_start();
     $rs = $stmh->fetchall();
     $count = 0;
     ?>
-    <div class="ms-5 px-1">
+    <div class="ms-5 me-10">
         <table class="table table-success table-striped">
             <thead>
                 <tr>
@@ -165,7 +165,7 @@ session_start();
                     }
                 }
                 ?>
-                
+
             </tbody>
         </table>
     </div>
