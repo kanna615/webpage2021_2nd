eb0464101c53f47a65352217f8c41ebc9c8eb1b3
kanna615 <e5429@gm.kochi-ct.jp>
2021-12-17 15:29:44 +0900

    deploy test

--- index.php
+++ index.php
@@ -134,13 +134,27 @@ session_start();
     foreach ($rs as $row) {
         if ($row['rane'] != "0") {
     ?>
-            <h3><?= $row['last_name'] ?>:　<font color="#ff0000"><?= $row['rane'] ?></font>レーン [<?= $row['work'] ?>]</h3>
+            <div class="container-fluid">
+                <!-- <h3><?= $row['last_name'] ?>:　<font color="#ff0000"><?= $row['rane'] ?></font>レーン [<?= $row['work'] ?>]</h3> -->
+                <table class="table table-dark table-info">
+                    <tbody>
+                        <tr>
+                            <td align='center'>{$row_['last_name']}</td>
+                            <td align='center'>{$row_['rane']}</td>
+                            <td align='center'>{$row_['work']}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
     <?php
             $count += 1;
         }
     }
     ?>
-    <h4>作業中の人数：<?= $count ?>人</h4>
+
+    <div class="container-fluid">
+        <h4>作業中の人数：<?= $count ?>人</h4>
+    </div>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
