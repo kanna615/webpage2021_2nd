b7da78754b4d168a7af74923ea77ff6511eddc17
kanna615 <e5429@gm.kochi-ct.jp>
2021-12-17 14:57:09 +0900

    deploy test

--- index.php
+++ index.php
@@ -96,7 +96,7 @@ session_start();
     <hr size="4" noshade>
     <div class="row">
         <div class="col-auto">
-            <div class="px-1 py-4 ml-2 mr-0 bg-primary"></div>
+            <div class="px-1 py-3 ml-2 mr-2 bg-primary"></div>
         </div>
 
         <div class="col-auto">
