a1490dd28be9f97a526a4916044294558528756e
kanna615 <e5429@gm.kochi-ct.jp>
2021-12-20 17:44:57 +0900

    deploy test

--- updateform.php
+++ updateform.php
@@ -116,10 +116,10 @@ session_start();
                     <div class="row justify-content-md-center">
                         <div class="col-10 mx-5 my-3">
                             <span class="border-top border-2"></span>
-                            <div class="d-flex justify-content-start mb-2">
+                            <!-- <div class="d-flex justify-content-start mb-2">
                                 <div class="p-2 bd-highlight">Flex item</div>
                                 <input type="text" name="last_name" value="<?= htmlspecialchars($row['last_name']) ?>">
-                            </div>
+                            </div> -->
                             <span class="border-top"></span>
                             <span class="border-top"></span>
                             <span class="border-top"></span>
